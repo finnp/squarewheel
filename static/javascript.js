c8121248c3c85f781085a782e2fdf4e5923ca204
Finn Pauls <derfinn@gmail.com>
2013-02-04 17:43:33 +0200

    Added prototype node-type filtering

--- static/javascript.js
+++ static/javascript.js
@@ -5,6 +5,12 @@ $(document).ready(function() {
     $("a#exploremuenchen").click(function() { load_venues('explore/muenchen') });
     $("a#lastcheckins").click(function() { load_venues('lastcheckins') });
     $("a#todo").click(function() { load_venues('todo') });
+    
+    $("#checkbox-wheelchair-unknown").click(function(){$(".wheelchair-unknown").toggle();} );
+    $("#checkbox-wheelchair-yes").click(function(){$(".wheelchair-yes").toggle();} );
+    $("#checkbox-wheelchair-no").click(function(){$(".wheelchair-no").toggle();} );
+    $("#checkbox-wheelchair-limited").click(function(){$(".wheelchair-limited").toggle();} );
+    
 });
 
 var load_venues = function(url) {
@@ -32,10 +38,12 @@ var load_wheelmap_info = function() {
         },
         success: function( r ) {
             if ( r.wheelmap ) {
+                $(div).addClass("wheelchair-" + r.wheelchair);
                 $(div).find('.nodeheadline').html(r.name);
                 $(div).find('.wheelmapinfo>p').html("<a href='http://wheelmap.org/nodes/" + r.wheelmap_id + "'>Wheelmap</a><br/>Wheelchair: " + r.wheelchair + "<br>Description: " + r.wheelchair_description);
                 $(div).find('.map img.mapmarker').attr("src", "http://wheelmap.org/marker/" + r.wheelchair + ".png")
             } else {
+                $(div).addClass("wheelchair-unknown");
                 $(div).find('.nodeheadline').html("Not found on wheelmap");
                 $(div).find('.wheelmapinfo>p').html("Look for it yourself on <a href='http://wheelmap.org/en/?lat="+$(div).data('lat')+"&lon="+$(div).data('lng')+"&zoom=17'>wheelmap</a>");
                 $(div).find('.map img.mapmarker').attr("src", "http://wheelmap.org/marker/unknown.png")
