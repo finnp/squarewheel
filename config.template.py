8f383886b3703d9b0ed887d7a115be4c82234093
Finn Pauls <derfinn@gmail.com>
2013-02-25 10:46:37 +0200

    Add template for the config file

---
+++ config.template.py
@@ -0,0 +1,21 @@
+WHEELMAP_API_KEY = ""
+# If you want to use the same Key for visitor edits and calls, 
+# then set this to False
+WHEELMAP_API_KEY_VISITOR_EDITS = ""
+
+FOURSQUARE_CLIENT_ID = ""
+FOURSQUARE_CLIENT_SECRET = ""
+FOURSQUARE_CALLBACK_URL = ""
+
+# Set this to a fixed random value, e.g. os.urandom(24)
+FLASK_SECRET_KEY = ""
+
+# MongoDB configuration
+MONGODB_HOST = 'localhost'
+MONGODB_PORT = 27017
+MONGODB_DBNAME = 'squarewheel'
+
+MONGODB_NAME = None
+MONGODB_PW = None
+
+DEBUG = True
