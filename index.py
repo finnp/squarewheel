f8cfa361d3dc3a6f42daa8f2e0ac254d8c0ca2ce
Finn <derfinn@gmail.com>
2014-08-12 18:04:15 +0200

    use env

--- index.py
+++ index.py
@@ -116,6 +116,6 @@ if __name__ == '__main__':
         app.run()
     else:
         # Bind to PORT if defined, otherwise default to 5000.
-        port = int(os.environ.get('PORT', 5000))
+        port = int(env.get('PORT', 5000))
         app.run(host='0.0.0.0', port=port)
     
